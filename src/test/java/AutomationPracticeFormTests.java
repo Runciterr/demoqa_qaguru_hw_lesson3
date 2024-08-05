dcb4fb6194f8962564437a377f537488ed9073d4
Runciter <unformatmail@gmail.com>
2024-08-05 08:50:28 +0300

    added fixes

--- src/test/java/AutomationPracticeFormTests.java
+++ src/test/java/AutomationPracticeFormTests.java
@@ -16,7 +16,7 @@ public class AutomationPracticeFormTests {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
+        //Configuration.holdBrowserOpen = true;
         Configuration.timeout = 5000; // default 4000
     }
     @AfterAll
