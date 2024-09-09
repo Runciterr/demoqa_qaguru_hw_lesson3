a9fb0d747069aaca80a144b1a61d77b6b36c3127
Runciter <unformatmail@gmail.com>
2024-09-09 13:47:31 +0300

    added SoftAssertionsJUnit5Test

---
+++ src/test/java/EnterprizeGitHubPageTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import com.codeborne.selenide.Configuration;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+public class EnterprizeGitHubPageTest {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://github.com";
+    }
+
+    @Test
+    void enterprisePageHaveHeadTest() {
+        open("");
+        $(byTagAndText("button", "Solutions")).hover();
+        $(byTagAndText("a", "Enterprise")).click();
+        $("#hero-section-brand-heading").shouldHave(text("The AI-powered developer platform."));
+    }
+}
