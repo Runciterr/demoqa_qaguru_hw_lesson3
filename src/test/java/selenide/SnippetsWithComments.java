ba4302f0cb1d9c2803e25a850cd8b2dd7cca4b6b
Runciter <unformatmail@gmail.com>
2025-06-19 12:58:03 +0300

    fixes

--- src/test/java/selenide/SnippetsWithComments.java
+++ src/test/java/selenide/SnippetsWithComments.java
@@ -13,7 +13,7 @@ import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 // this is not a full list, just the most common
-public class Snippets {
+public class SnippetsWithComments {
 
   void browser_command_examples() {
     open("https://google.com");
