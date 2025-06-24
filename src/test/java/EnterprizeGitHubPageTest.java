b06a14ddcfb8ec5000dcc3e33cfab3d290970d9c
Runciter <unformatmail@gmail.com>
2025-06-24 13:15:43 +0300

    fixes

--- src/test/java/EnterprizeGitHubPageTest.java
+++ src/test/java/EnterprizeGitHubPageTest.java
@@ -1,5 +1,6 @@
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import com.codeborne.selenide.Configuration;
 import static com.codeborne.selenide.Condition.text;
@@ -11,18 +12,18 @@ public class EnterprizeGitHubPageTest {
     @BeforeAll
     static void setUp() {
         Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://github.com";
     }
 
     @Test
     void enterprisePageHaveHeadTest() {
-        open("");
+        open("https://github.com");
         $(byTagAndText("button", "Solutions")).hover();
         $(byTagAndText("a", "Enterprise")).click();
         $("#hero-section-brand-heading").shouldHave(text("The AI-powered\n" + "developer platform"));
     }
+
     @Test
-    void dragAndDropTest() {
+    void ActionsTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
         SelenideElement source = $("#column-a");
         SelenideElement target = $("#column-b");
@@ -36,10 +37,19 @@ public class EnterprizeGitHubPageTest {
         source.shouldHave(text("B"));
         target.shouldHave(text("A"));
 
-        //проверка работает ли тест, с командой dragAndDrop
-        source.dragAndDrop(to("#column-b"));
-        //проверка того, что headers элементов в первоначальном состоянии
+    }
+    @Test
+    void dragAndDropTest() {
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        SelenideElement source = $("#column-a");
+        SelenideElement target = $("#column-b");
+
         source.shouldHave(text("A"));
         target.shouldHave(text("B"));
+
+        source.dragAndDrop(to(target));
+
+        source.shouldHave(text("B"));
+        target.shouldHave(text("A"));
     }
 }
