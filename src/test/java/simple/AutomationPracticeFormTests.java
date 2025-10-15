f6846adc0e728b7aafb6e4a4b0888f9d5433c760
Runciter <unformatmail@gmail.com>
2025-10-15 22:29:49 +0300

    some fixes

--- src/test/java/simple/AutomationPracticeFormTests.java
+++ src/test/java/simple/AutomationPracticeFormTests.java
@@ -20,6 +20,7 @@ public class AutomationPracticeFormTests {
         Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
 
     @AfterAll
