26121d845154d5347cc1a9140e7043acdd9472e3
Runciter <unformatmail@gmail.com>
2025-10-15 22:41:46 +0300

    some fixes

--- src/test/java/simple/AutomationPracticeFormTests.java
+++ src/test/java/simple/AutomationPracticeFormTests.java
@@ -2,6 +2,8 @@ package simple;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
@@ -12,6 +14,7 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
 
+
 @Tag("demoqa")
 public class AutomationPracticeFormTests {
 
@@ -21,6 +24,7 @@ public class AutomationPracticeFormTests {
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @AfterAll
